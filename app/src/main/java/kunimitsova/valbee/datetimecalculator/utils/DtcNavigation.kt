08580392988e3496c11861121b55a0605f481d7c
kunimitsova <valla5@verizon.net>
2022-08-23 12:20:53 +0900

    update navigation
    update navigation so that calling Help screen doesn't make the
    back button optiosn weird. but, that made it so every time
    you click back button, it goes to home screen. That's OK since
    the state of the screens are saved so if you were on DateDiff
    you can just click DateDiff again. This is not perfect but
    it's acceptable for now.

--- app/src/main/java/kunimitsova/valbee/datetimecalculator/utils/DtcNavigation.kt
+++ app/src/main/java/kunimitsova/valbee/datetimecalculator/utils/DtcNavigation.kt
@@ -1,7 +1,87 @@
 package kunimitsova.valbee.datetimecalculator.utils
 
-object DtcNavigation {
-    const val ADD_DAY_SCREEN = "DateTimeScreen"
-    const val SUB_DATE_SCREEN = "SubtractDateScreen"
-    const val HELP_SCREEN = "HelpScreen"
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import kunimitsova.valbee.datetimecalculator.R
+import kunimitsova.valbee.datetimecalculator.ui.screens.DateDifferenceScreen
+import kunimitsova.valbee.datetimecalculator.ui.screens.DateTimeScreen
+import kunimitsova.valbee.datetimecalculator.ui.screens.HelpScreen
+
+sealed class Screen( val route: String, @StringRes val resourceId: Int) {
+    object addScreen: Screen("addscreen", R.string.add_date)
+    object dateDiff: Screen("datediff", R.string.date_difference)
+    object helpScreen: Screen("help", R.string.help)
+}
+
+val mainLocations = listOf(
+    Screen.addScreen,
+    Screen.dateDiff
+)
+
+@Composable
+fun DtcNavHost(
+    navController: NavHostController,
+    startDestination: String,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(Screen.addScreen.route) {
+            DateTimeScreen()
+        }
+        composable(Screen.dateDiff.route ) {
+            DateDifferenceScreen()
+        }
+        composable(Screen.helpScreen.route ) {
+            HelpScreen()
+        }
+    }
+}
+
+fun NavHostController.navigatePopToHome(route: String) =
+    this.navigate(route) {
+        // Pop up to the start destination of the graph to
+        // avoid building up a large stack of destinations
+        // on the back stack as users select items
+        if (this@navigatePopToHome.previousBackStackEntry?.destination?.route == Screen.helpScreen.route) {
+            this@navigatePopToHome.backQueue.removeLastOrNull()
+        }
+        popUpTo(
+            this@navigatePopToHome.graph.findStartDestination().id
+        ) {
+            saveState = true
+        }
+        // Avoid multiple copies of the same destination when
+        // reselecting the same item
+        launchSingleTop = true
+        // Restore state when reselecting a previously selected item
+        restoreState = true
+    }
+fun NavHostController.navigateSingleTopTo(route:String) {
+    // navigate so that only one of any given screen is in the stack
+        this.navigate(route) {
+            launchSingleTop = true
+        }
+}
+
+fun NavHostController.navigateWithPopUp(
+    toRoute: String,  // route name where you want to navigate
+    fromRoute: String // route you want from popUpTo.
+) {
+    this.navigate(toRoute) {
+        popUpTo(fromRoute) {
+            inclusive = true
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
+    }
 }
